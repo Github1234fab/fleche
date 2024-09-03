18f4df7fc8c23944587f46eb0b5e77534a33a5fc
Github1234fab <fabien.marceau@gmail.com>
2024-09-03 12:05:01 +0200

    copie des fichiers lats version

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+        import Header from "../components/Header.svelte";
+        import Footer from "../components/Footer.svelte";
+</script>
+
+<main>
+        <Header />
+        <slot />
+        <Footer />
+</main>
+
+<style>
+        main {
+                min-height: 100vh;
+        }
+</style>
---
+++ src/routes/Brindas/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+        import Services from "../../components/Services.svelte";
+</script>
+
+<main class="brignais">
+        <section class="header">
+                <h1>Brignais</h1>
+        </section>
+        <section class="services">
+                <Services />
+        </section>
+</main>
